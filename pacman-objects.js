179a77d32dfeb1e908b0e1311f3af41127429fcc
Blueibis <jennie.li.2011@gmail.com>
2017-06-29 15:59:29 -0400

    create pacman-objects.js

---
+++ pacman-objects.js
@@ -0,0 +1,11 @@
+var inky = {};
+inky.colour = 'Cyan';
+inky.personality = 'Shadow';
+console.log(inky);
+
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+console.log(ghosts);
+console.log(ghosts.inky);
+console.log(ghosts['inky']);
+var ghostName = 'inky';
+console.log(ghosts[ghostName]);
